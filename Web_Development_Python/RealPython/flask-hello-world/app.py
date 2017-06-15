3179cbef56c8124ad019457005e1b27b77a24a38
nitin-cherian <nitin.cherian@gmail.com>
2017-06-15 06:58:29 +0530

    Learn dynamic routes in flask.

--- Web_Development_Python/RealPython/flask-hello-world/app.py
+++ Web_Development_Python/RealPython/flask-hello-world/app.py
@@ -16,6 +16,32 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+
+# dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+
+@app.route('/integer/<int:value>')
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+
+@app.route('/float/<float:value>')
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+
+# dynamic route that accepts slashes
+@app.route('/path/<path:value>')
+def path_type(value):
+    print(value)
+    return "correct"
+
+
 # start the development server using the run() method
 if __name__ == '__main__':
     app.run()
