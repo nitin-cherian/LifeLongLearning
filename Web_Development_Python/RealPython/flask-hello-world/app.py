223a16b92af06a59c70b1e493af0f8864a3b8c51
nitin-cherian <nitin.cherian@gmail.com>
2017-06-26 19:58:23 +0530

    Relearn the hello world flask app.

--- Web_Development_Python/RealPython/flask-hello-world/app.py
+++ Web_Development_Python/RealPython/flask-hello-world/app.py
@@ -1,18 +1,17 @@
-# ------ Flask Hello World ------- #
-
+# ---- Flask Hello World ---- #
 
 # import the Flask class from the flask package
 from flask import Flask
+
 
 # create the application object
 app = Flask(__name__)
 
 
-# use the decorator pattern to
-# link the view function to a url
+# use the decorator pattern to link the view function to a url
 @app.route('/')
 @app.route('/hello')
-# define the view using a function, which returns a string
+# define the view using a function which returns a string
 def hello_world():
     return "Hello, World!"
 
@@ -23,15 +22,17 @@ def search(search_query):
     return search_query
 
 
+# dynamic route that identifies integer
 @app.route('/integer/<int:value>')
 def int_type(value):
-    print(value + 1)
+    print(value+1)
     return "correct"
 
 
+# dynamic route that identifies float
 @app.route('/float/<float:value>')
 def float_type(value):
-    print(value + 1)
+    print(value+1)
     return "correct"
 
 
@@ -42,7 +43,6 @@ def path_type(value):
     return "correct"
 
 
-# start the development server using the run() method
+# start the development server using the run method
 if __name__ == '__main__':
     app.run()
-
