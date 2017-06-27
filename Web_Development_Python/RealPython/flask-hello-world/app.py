670bc6667057e00b319b536aee92a5f3525d9e0e
nitin-cherian <nitin.cherian@gmail.com>
2017-06-27 06:34:06 +0530

    Learn about response object and debug mode in Flask.

--- Web_Development_Python/RealPython/flask-hello-world/app.py
+++ Web_Development_Python/RealPython/flask-hello-world/app.py
@@ -8,6 +8,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# error handling
+app.config["DEBUG"] = True
+
+
 # use the decorator pattern to link the view function to a url
 @app.route('/')
 @app.route('/hello')
@@ -41,6 +45,14 @@ def float_type(value):
 def path_type(value):
     print(value)
     return "correct"
+
+
+@app.route('/name/<name>')
+def index(name):
+    if name.lower() == "nitin":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
 
 
 # start the development server using the run method
