fbe645d614ebaf48dba1b5f46a909ce1bc44a04a
nitin-cherian <nitin.cherian@gmail.com>
2017-07-26 10:45:02 +0530

    Make a custom container object supported by magic methods

--- Python/PythonProgrammingLanguage/Python_Magic_Methods/holding.py
+++ Python/PythonProgrammingLanguage/Python_Magic_Methods/holding.py
@@ -23,6 +23,42 @@ class Holding:
         self.shares -= nshares
 
 
+class Portfolio:
+    def __init__(self):
+        self.holdings = []
+
+    @classmethod
+    def from_csv(cls, filename):
+        self = cls()
+        with open(filename, "r") as f:
+            rows = csv.reader(f)
+            header = next(rows)  # skip the header
+            for row in rows:
+                h = Holding(row[0], row[1], int(row[2]), float(row[3]))
+                self.holdings.append(h)
+
+        return self
+
+    def __len__(self):
+        return len(self.holdings)
+
+    def __getitem__(self, item):
+        if isinstance(item, str):
+            return [h for h in self.holdings if h.name == item]
+        else:
+            return self.holdings[item]
+
+    def __iter__(self):
+        return self.holdings.__iter__()  # Rely on the __iter__ method of the native list object
+
+    def total_cost(self):
+        return sum([h.shares * h.price for h in self.holdings])
+
+    def current_value(self):
+        pass
+
+
+
 def read_portfolio(filename):
     with open(filename, "r") as f:
         rows = csv.reader(f)
