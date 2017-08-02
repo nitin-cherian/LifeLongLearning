92fd459ff3e64c04ad8ec950a59bdc3a82f592b5
nitin-cherian <nitin.cherian@gmail.com>
2017-08-02 07:30:19 +0530

    Create the basic login, main and logout functions

--- Web_Development_Python/RealPython/flask-blog/blog.py
+++ Web_Development_Python/RealPython/flask-blog/blog.py
@@ -6,14 +6,14 @@ from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
 
-
 # configuration
 DATABASE = "blog.db"
-
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "hard_to_guess"
 
 # Create an instance of Flask class
 app = Flask(__name__)
-
 
 # pulls in app configurations by looking for UPPERCASE variables
 app.config.from_object(__name__)
@@ -24,7 +24,32 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                        request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid Credentials. Please Try again!"
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for("main"))
+    return render_template("login.html", error=error), status_code
+
+
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash("You were logged out")
+    return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
