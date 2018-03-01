5d5774bd6cf494877dabdc8cab1fdda63ebb9cf8
nitin-cherian <nitin.cherian@gmail.com>
2018-03-01 06:28:53 +0530

    Chapter 1: Hello, World! (v0.1)

---
+++ Python/Python_Web_Development/FlaskMegaTutorial/microblog/microblog.py
@@ -0,0 +1,6 @@
+from app import app
+# The code in the below comment works. Added for understanding.
+# from app import routes
+# print(routes.index())
+# from app import Flask
+# print(Flask.__doc__)
