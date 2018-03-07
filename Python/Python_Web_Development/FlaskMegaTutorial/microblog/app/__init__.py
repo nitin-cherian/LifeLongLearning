c2b6a963ab30dc324d77b829300d4e722684f96e
nitin-cherian <nitin.cherian@gmail.com>
2018-03-07 20:23:58 +0530

    Chapter 3: Web Forms (v0.3)

--- Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/__init__.py
+++ Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/__init__.py
@@ -3,8 +3,11 @@ When you import a package, the __init__.py executes and
 defines what symbols it exposes to the outside world.
 """
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # from . import routes   # This is same as 'from app import routes'
 from app import routes
