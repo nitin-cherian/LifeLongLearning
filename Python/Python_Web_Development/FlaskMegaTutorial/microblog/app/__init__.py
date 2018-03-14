059f1570cfde6eb9669d29a4480e4d6daf4cf500
nitin-cherian <nitin.cherian@gmail.com>
2018-03-14 07:16:50 +0530

    Merge database branch onto master

--- Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/__init__.py
+++ Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/__init__.py
@@ -4,10 +4,14 @@ defines what symbols it exposes to the outside world.
 """
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)  # represents the database
+migrate = Migrate(app, db)  # represents the migration engine
 
 # from . import routes   # This is same as 'from app import routes'
-from app import routes
+from app import routes, models
