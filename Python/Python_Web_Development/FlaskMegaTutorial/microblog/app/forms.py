a714759eeb965e26296338119d56ad552e47fdff
nitin-cherian <nitin.cherian@gmail.com>
2018-03-29 11:44:45 +0530

    Chapter 6: Profile Page and Avatars (v0.6)

--- Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/forms.py
+++ Python/Python_Web_Development/FlaskMegaTutorial/microblog/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 
 from app.models import User
 
@@ -28,3 +28,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+
